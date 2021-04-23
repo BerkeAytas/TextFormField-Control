0d3494c60997be5923560c7c12c1ddc3b806b3a9
BerkeAytas <berkeaytass@gmail.com>
2021-04-23 17:51:25 +0300

    TextFormField Control

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Form-TextFormField',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'TextFormField'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Username'),
+              validator: (val) {
+                if (val.length < 6) {
+                  if (val.isEmpty) {
+                    return 'Kullanıcı Adınızı Giriniz';
+                  } else {
+                    return 'Kullanıcı Uzunluğu Min.6 Karakter Uzunluğunda Olmalıdır';
+                  }
+                } else {
+                  return null;
+                }
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              validator: (val) {
+                if (val.isEmpty) {
+                  return 'Şifrenizi Girin';
+                } else {
+                  return null;
+                }
+              },
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  _formKey.currentState.reset(); //Login olduktan sonra state sıfırlamak için kullanılıyor.
+                  if (_formKey.currentState.validate()) {
+                    print("oke");
+                  } else {
+                    print("error");
+                  }
+                },
+                child: Text("Giriş Yap"))
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _formKey.currentState.dispose();
+    super.dispose();
+  }
+}
